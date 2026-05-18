a5a740a5ff964aef056fb4dd04a661d143d96a83
jannatulfikri <jannatulfikri4@gmail.com>
2026-05-18 19:21:20 +0700

    Tugas 18 mei 26, Transportasi

---
+++ Tugas 18 mei 26, Transportasi/mobil.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas180526;
+
+/**
+ *
+ * @author janna
+ */
+public class mobil extends transportasiOnline{
+    private double biaya;
+    private String tujuan;
+
+    public mobil(double biaya, String tujuan, String nama) {
+        super(nama);
+        this.biaya = biaya;
+        this.tujuan = tujuan;
+    }
+
+    public double getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(double biaya) {
+        this.biaya = biaya;
+    }
+
+    public String getTujuan() {
+        return tujuan;
+    }
+
+    public void setTujuan(String tujuan) {
+        this.tujuan = tujuan;
+    }
+    
+    
+    
+    @Override
+    public void jenisTransport(){
+        System.out.println("Perjalanan menggunakan Mobil");
+        System.out.println("nama pelanggan: "+nama);
+        System.out.println("Tujuan Perjalanan: "+tujuan);
+        System.out.println("Biaya Perjalanan: "+biaya);
+    }
+}
